f8176dd2a156c04848968614dde555c8e3c1588f
basith670 <basithkdrmf87@gmail.com>
2026-08-11 12:00:18 +0530

    Separate Express app from server startup

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,11 +1,8 @@
 require('dotenv').config()
 
-const express = require('express')
 const mongoose = require('mongoose')
 
-const Blog = require('./models/blog')
-
-const app = express()
+const app = require('./app')
 
 const mongoUrl = process.env.MONGODB_URI
 
@@ -17,22 +14,6 @@ mongoose
   .catch(error => {
     console.log('error connecting to MongoDB:', error.message)
   })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then(result => {
-    response.status(201).json(result)
-  })
-})
 
 const PORT = process.env.PORT || 3003
 
